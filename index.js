570f46b32c17340f93f3c5f72d45a01141cf84d1
profex55 <profex55@gmail.com>
2020-09-15 13:05:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+/*let para = document.querySelector('p');
+let nPara = prompt ('Whats your name?!');
+if (nPara.length > 0){
+para.textContent = 'Welcome '+ nPara;
+}
+
+let nColor = prompt ('Enter your favorite color');
+if (nColor.length > 0){
+para.style.color = nColor;
+}
+
+let nSize = prompt ('Enter a size of paragraph');
+para.style.fontSize = nSize+'px';
+*/
+
